3152dcf84d1755584946c759074284f62b747fcd
Hirofumi Wakasugi <baenej@gmail.com>
2018-08-16 12:35:09 +0900

    Update rules

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import Person from "./classes/person"
 
 const person = new Person("Tom")
-// tslint:disable-next-line:no-console
 console.log(person.hello())
-// tslint:disable-next-line:no-console
 console.log(person.whereAmI())
