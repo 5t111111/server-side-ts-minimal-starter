6eda0be6fe3809baf17466e042c0b46881733eb3
Hirofumi Wakasugi <baenej@gmail.com>
2018-08-16 12:27:48 +0900

    Update example code

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
-function hello(name: string): boolean {
-  // tslint:disable-next-line:no-console
-  console.log(`Hello ${name}!`)
-  return true
+import * as path from "path"
+
+class Person {
+  constructor(private name: string) {}
+
+  public hello() {
+    return `Hello, ${this.name}!`
+  }
+
+  public whereAmI() {
+    return `I'm in ${path.dirname(__dirname)}`
+  }
 }
 
-hello("Tom")
+const person = new Person("Tom")
+// tslint:disable-next-line:no-console
+console.log(person.hello())
+// tslint:disable-next-line:no-console
+console.log(person.whereAmI())
